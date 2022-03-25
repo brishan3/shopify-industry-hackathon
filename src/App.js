3e78d72c7de504c1af9f5c468eb781095cb3d4b7
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-03-25 14:31:00 -0400

    removed router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import "./App.scss";
-import LandingPage from "./pages/LandingPage/LandingPage";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Main from "./components/Main/Main";
+
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" component={Main} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <>
+      <Main/>
+    </>
   );
 }
 
