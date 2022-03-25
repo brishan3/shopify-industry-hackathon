c47496f495967557b23a148ed302c5533835f02a
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-03-25 13:52:40 -0400

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import "./App.scss";
-import ReactFullpage from "@fullpage/react-fullpage";
-// import { BrowserRouter, Switch, Route } from 'react-router-dom'
-// import PageHeader from './components/PageHeader/PageHeader'
 import LandingPage from "./pages/LandingPage/LandingPage";
-import PreviewPage from "./pages/PreviewPage/PreviewPage";
-import OnboardingPage from "./pages/OnboardingPage/OnboardingPage";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import PageHeader from "./components/PageHeader/PageHeader";
-import Onboard from "./components/Onboard/Onboard";
-import onboardOne from "./assets/images/onboardOne.svg";
-import onboardTwo from "./assets/images/onboardTwo.svg";
-import onboardThree from "./assets/images/onboardThree.svg";
-import onboardFour from "./assets/images/onboardFour.svg";
-import stepperOne from "./assets/images/stepperOne.svg";
-import stepperTwo from "./assets/images/stepperTwo.svg";
-import stepperThree from "./assets/images/stepperThree.svg";
-import stepperFour from "./assets/images/stepperFour.svg";
 import Main from "./components/Main/Main";
 
 function App() {
@@ -31,3 +16,5 @@ function App() {
 }
 
 export default App;
+
+
